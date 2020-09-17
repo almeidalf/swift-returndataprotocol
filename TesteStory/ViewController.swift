9a7ab3aecd7357261c6e924a1a06c203076d142d
almeidalf <felipedovale1997@gmail.com>
2020-09-17 15:24:39 -0300

    Initial Commit

---
+++ TesteStory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TesteStory
+//
+//  Created by Reply on 17/09/20.
+//  Copyright © 2020 storydelegate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
